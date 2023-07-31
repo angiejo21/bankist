99d44a582df40a00112d873229f2f8bffa487041
Angela Bellò <angela.bello90@gmail.com>
2023-07-31 19:57:05 +0200

    >> project: set up folder

---
+++ lectures.js
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/*
+/////////////////////////////////////////////////
+//SLICE(non modifica l'array)
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+//SPLICE (modifica l'array, il secondo argomento è il numero di elem da eliminare)
+// console.log(arr.splice(2));
+arr.splice(1, 2);
+console.log(arr);
+//REVERSE (modifica l'array)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+//CONCAT (non modifica gli array)
+const letters = arr.concat(arr2);
+console.log(...arr, ...arr2);
+console.log(letters);
+//JOIN
+console.log(letters.join('-'));
+//AT
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const move of movements)
+  for (const [i, move] of movements.entries()) {
+    if (move > 0) {
+      console.log(`Movement ${i + 1}: You deposited ${move}`);
+    } else {
+      console.log(`Movement ${i + 1}: You withdrew ${Math.abs(move)}`);
+    }
+  }
+console.log('---FOR EACH---');
+movements.forEach(function (move, i) {
+  if (move > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${move}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(move)}`);
+  }
+});
+//Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (cValue, cKey, map) {
+  console.log(`${cKey}: ${cValue}`);
+});
+//Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+  */
